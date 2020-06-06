f4f99280e88d8ebed4ca105f5c9dccfc03838916
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-06 06:27:41 +0000

    Deploy Cary-Xx/Full-Travis-Tutorial to github.com/Cary-Xx/Full-Travis-Tutorial.git:gh-pages

--- src/test/java/SUTTest.java
+++ src/test/java/SUTTest.java
@@ -1,11 +1,12 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Test;
 
 public class SUTTest {
 
     @Test
-    public void testStringConcatenation_twoEmptyStrings_typeI() throws Exception {
+    public void testStringConcatenation_twoEmptyStrings_typeI() {
         String s1 = "";
         String s2 = "";
         SUT sut = new SUT();
@@ -14,7 +15,7 @@ public class SUTTest {
     }
 
     @Test(expected = NullPointerException.class)
-    public void testStringConcatenation_nullStringS1_throwsNullPointerException() throws Exception {
+    public void testStringConcatenation_nullStringS1_throwsNullPointerException() {
         String s1 = null;
         String s2 = "";
         SUT sut = new SUT();
@@ -22,7 +23,15 @@ public class SUTTest {
     }
 
     @Test
-    public void testStringConcatenation_length2_typeI() throws Exception {
+    public void testStringConcatenation_nullStringS1_throwsNullPointerException2() {
+        String s1 = null;
+        String s2 = "";
+        SUT sut = new SUT();
+        assertThrows(NullPointerException.class, () -> sut.stringConcatenation(s1, s2));
+    }
+
+    @Test
+    public void testStringConcatenation_length2_typeI() {
         String s1 = "a";
         String s2 = "b";
         SUT sut = new SUT();
@@ -31,7 +40,7 @@ public class SUTTest {
     }
 
     @Test
-    public void testStringConcatenation_length5_typeII() throws Exception {
+    public void testStringConcatenation_length5_typeII() {
         String s1 = "abc";
         String s2 = "de";
         SUT sut = new SUT();
@@ -40,7 +49,7 @@ public class SUTTest {
     }
 
     @Test
-    public void testStringConcatenation_length9_typeIII() throws Exception {
+    public void testStringConcatenation_length9_typeIII() {
         String s1 = "abcde";
         String s2 = "fghi";
         SUT sut = new SUT();
@@ -49,7 +58,7 @@ public class SUTTest {
     }
 
     @Test
-    public void testStringConcatenation_length18_typeIV() throws Exception {
+    public void testStringConcatenation_length18_typeIV() {
         String s1 = "abcdefghi";
         String s2 = "jklmnopqr";
         SUT sut = new SUT();
@@ -58,7 +67,7 @@ public class SUTTest {
     }
 
     @Test
-    public void testStringConcatenation_length26_typeV() throws Exception {
+    public void testStringConcatenation_length26_typeV() {
         String s1 = "abcdefghijklmn";
         String s2 = "opqrstuvwxyz";
         SUT sut = new SUT();
@@ -67,7 +76,7 @@ public class SUTTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testStringConcatenation_length49_throwsIllegalArgumentException() throws Exception {
+    public void testStringConcatenation_length49_throwsIllegalArgumentException() {
         String s1 = "Greetings from Intellij. ";
         String s2 = "Greetings from Eclipse. ";
         SUT sut = new SUT();
@@ -75,7 +84,7 @@ public class SUTTest {
     }
 
     @Test
-    public void testStringConcatenation_twoSpecialChar_typeI() throws Exception {
+    public void testStringConcatenation_twoSpecialChar_typeI() {
         String s1 = "\n";
         String s2 = "\r";
         SUT sut = new SUT();
